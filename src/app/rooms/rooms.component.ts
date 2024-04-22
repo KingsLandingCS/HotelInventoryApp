3fb91c1a323845957f0c910fb146dca6ae089256
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-22 21:48:16 +0500

    learning

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
 export class RoomsComponent {
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
+  hideRooms = false;
   toggle() {
-
+    this.hideRooms = !this.hideRooms;
   }
 }

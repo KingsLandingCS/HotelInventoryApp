c4a917f4f62cede87b80386f99e734beaed018ff
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-23 12:32:52 +0500

    learning

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -15,6 +15,36 @@ export class RoomsComponent {
     availableRooms: 10,
     bookedRooms: 5,
   };
+
+  roomList: RoomList[] = [
+    {
+      roomType: 'Deluxe Room',
+      amenities: 'Swiming Pool, Gym, Spa, Sports Complex, Hi Tea, Buffet',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      checkInTime: new Date('11-Apr-2024'),
+      checkOutTime: new Date('23-Apr-2022'),
+    },
+    {
+      roomType: 'Deluxe Room',
+      amenities: 'Swiming Pool, Gym, Spa, Sports Complex, Hi Tea, Buffet',
+      price: 1500,
+      photos:
+        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      checkInTime: new Date('11-Apr-2024'),
+      checkOutTime: new Date('23-Apr-2022'),
+    },
+    {
+      roomType: 'Private Suite',
+      amenities: 'Swiming Pool, Gym, Spa, Sports, Complex, Hi Tea, Buffet',
+      price: 5000,
+      photos:
+        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      checkInTime: new Date('11-Apr-2024'),
+      checkOutTime: new Date('23-Apr-2022'),
+    },
+  ];
   toggle() {
     this.hideRooms = !this.hideRooms;
   }

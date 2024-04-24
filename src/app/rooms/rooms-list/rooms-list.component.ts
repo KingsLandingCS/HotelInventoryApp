9ac25e57dc25e11690cccea0e971226567cad71b
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-24 14:49:07 +0500

    learning

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,16 +1,30 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnInit, OnChanges {
   @Input() rooms: RoomList[] = [];
+  @Input() title: string = '';
   @Output() selectedRoom = new EventEmitter<RoomList>();
 
   constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
@@ -18,5 +32,4 @@ export class RoomsListComponent implements OnInit {
   selectRoom(room: RoomList) {
     this.selectedRoom.emit(room);
   }
-
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -10,11 +10,14 @@ export class RoomsComponent {
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
   hideRooms = false;
+  selectedRoom!: RoomList;
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
   };
+
+  title = 'Room List';
 
   roomList: RoomList[] = [];
 
@@ -59,8 +62,26 @@ export class RoomsComponent {
   }
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = 'Rooms List';
   }
   selectRoom(room: RoomList) {
-    console.log(room);
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenities: 'Swiming Pool, Gym, Spa, Sports-Complex, Hi Tea, Buffet',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      checkInTime: new Date('11-Apr-2024'),
+      checkOutTime: new Date('23-Apr-2022'),
+      rating: 4.5,
+    };
+
+    // this.roomList.push(room);
+    this.roomList = [...this.roomList, room];
   }
 }

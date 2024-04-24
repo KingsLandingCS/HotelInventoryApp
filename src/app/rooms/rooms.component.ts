c3080e656658dfef51896b877f68313287cf3458
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-24 11:41:46 +0500

    learning

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -16,41 +16,48 @@ export class RoomsComponent {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Swiming Pool, Gym, Spa, Sports Complex, Hi Tea, Buffet',
-      price: 1000,
-      photos:
-        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      checkInTime: new Date('11-Apr-2024'),
-      checkOutTime: new Date('23-Apr-2022'),
-      rating: 4.5,
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Swiming Pool, Gym, Spa, Sports Complex, Hi Tea, Buffet',
-      price: 1500,
-      photos:
-        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      checkInTime: new Date('11-Apr-2024'),
-      checkOutTime: new Date('23-Apr-2022'),
-      rating: 3.4,
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'Swiming Pool, Gym, Spa, Sports, Complex, Hi Tea, Buffet',
-      price: 5000,
-      photos:
-        'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      checkInTime: new Date('11-Apr-2024'),
-      checkOutTime: new Date('23-Apr-2022'),
-      rating: 3.8,
-    },
-  ];
+  roomList: RoomList[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Swiming Pool, Gym, Spa, Sports-Complex, Hi Tea, Buffet',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        checkInTime: new Date('11-Apr-2024'),
+        checkOutTime: new Date('23-Apr-2022'),
+        rating: 4.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Swiming Pool, Gym, Spa, Sports-Complex, Hi Tea, Buffet',
+        price: 1500,
+        photos:
+          'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        checkInTime: new Date('11-Apr-2024'),
+        checkOutTime: new Date('23-Apr-2022'),
+        rating: 3.4,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Swiming Pool, Gym, Spa, Sports-Complex, Hi Tea, Buffet',
+        price: 5000,
+        photos:
+          'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        checkInTime: new Date('11-Apr-2024'),
+        checkOutTime: new Date('23-Apr-2022'),
+        rating: 3.8,
+      },
+    ];
+  }
+
   toggle() {
     this.hideRooms = !this.hideRooms;
   }

ebd430d083719ccd965192285b4ba42389920def
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-25 16:40:02 +0500

    learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'HotelInventoryApp';
 
-  role = 'User';
+  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+
+  ngAfterViewInit() {
+    const componentRef = this.vcr.createComponent(RoomsComponent);
+  }
 }
